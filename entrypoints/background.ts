5454b5da404881be04dbe3beadba3176044f0413
mikoloism <mikoloism@gmail.com>
2025-03-13 23:44:20 +0330

    refactor(entrypoints/background): remove unnecessary injection

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,11 +1,3 @@
-import HardwareMocker from "../packages/hardware/mod";
-import TimeZoneMocker from "../packages/timezone/mod";
-import UserAgentMocker from "../packages/user-agent/mod";
-
 export default defineBackground(() => {
   console.log("Hello background!", { id: browser.runtime.id });
-  const hardware = HardwareMocker.mockRandom();
-  const timeZone = TimeZoneMocker.mockRandom();
-  const userAgent = UserAgentMocker.mockRandom();
-  console.log({ hardware, timeZone, userAgent });
 });
