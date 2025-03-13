b9df163ebc685bec1a8689473d6fcf70da7ea3af
mikoloism <mikoloism@gmail.com>
2025-03-13 23:44:44 +0330

    refactor(content): improve injection and definition

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -3,11 +3,23 @@ import TimeZoneMocker from "../packages/timezone/mod";
 import UserAgentMocker from "../packages/user-agent/mod";
 
 export default defineContentScript({
-  matches: ["*://*/*"],
-  async main() {
+  matches: ["*://*/*", "<all_urls>"],
+  runAt: "document_start",
+  main() {
+    const customScript = document.createElement("script");
+    customScript.textContent = 'console.log("Hello");';
+    document.head.prepend(customScript);
+    console.log("[PASAD] :: Start on(document_start)");
     const hardware = HardwareMocker.mockRandom();
     const timeZone = TimeZoneMocker.mockRandom();
     const userAgent = UserAgentMocker.mockRandom();
+    Object.defineProperty(window, "pasad", {
+      configurable: false,
+      enumerable: false,
+      writable: false,
+      value: true,
+    });
     console.log({ hardware, timeZone, userAgent });
+    console.log("[PASAD] :: Finish on(document_start)");
   },
 });
